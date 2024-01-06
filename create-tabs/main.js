68928458fc02d44a4889678954a5fb37a464b4fe
borkboreiro <153937008+borkboreiro@users.noreply.github.com>
2024-01-06 18:35:38 +0300

    Add files via upload

---
+++ create-tabs/main.js
@@ -0,0 +1,17 @@
+let tabs = document.querySelectorAll(".tabs li");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".content > div");
+let divsArray = Array.from(divs);
+
+tabsArray.forEach((ele) => {
+  ele.addEventListener("click", function (e) {
+    tabsArray.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    divsArray.forEach((div) => {
+      div.style.display = "none";
+    });
+    document.querySelector(e.currentTarget.dataset.cont).style.display = "block";
+  });
+});
